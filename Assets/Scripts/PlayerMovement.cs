74db7d1472c3c635b1889d146570097b2adc0191
RamonBoelens <ramonboelens@hotmail.com>
2018-10-11 14:27:25 +0200

    Implemented PlayerMovement
    Player movement implemented with WASD or Arrow keys.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,36 +6,40 @@ public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
     [Range(1, 5)]
-    float speed = 1;
+    float moveSpeed = 4f;
+
+    Vector3 forward, right;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        Move();
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+            Move();
     }
 
     // Check for player input and move accordingly
     private void Move()
     {
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime);
-        }
-        if (Input.GetAxis("Vertical") > 0)
-        {
-            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
-        }
-        if (Input.GetAxis("Vertical") < 0)
-        {
-            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
-        }
+        Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        // Create direction
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        // Rotation
+        transform.forward = heading;
+
+        // Movement
+        transform.position += rightMovement;
+        transform.position += upMovement;
     }
 }
