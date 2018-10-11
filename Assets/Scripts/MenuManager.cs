6bac340044e207da30f1e6e247dcfe96ae4ca08e
RamonBoelens <ramonboelens@hotmail.com>
2018-10-11 13:34:55 +0200

    Basic Game
    --

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    // Access point for other scripts
+    public static MenuManager instance;
+
+    private void Awake()
+    {
+        Debug.Log("Menu instance: " + instance);
+
+        // Singleton
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        // Keep this game object alive while going through different scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SwitchScene(int _sceneIndex)
+    {
+        SceneManager.LoadScene(_sceneIndex);
+    }
+
+    public void CloseProgram()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    [Range(1, 5)]
+    float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+
+    // Check for player input and move accordingly
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(new Vector3(0, 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime);
+        }
+    }
+}
